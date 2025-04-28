553f3c5c0a67bcf784d666c6f0687384932ce665
_v <caocuongttn2@gmail.com>
2025-04-28 11:29:12 +0700

    [PostgreSQL] use PostgreSQL database with AWS

--- firstdjango/settings.py
+++ firstdjango/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'firstdjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'masteruser',
+        'PASSWORD': 'Qsongdev123',
+        'HOST': 'w3-django-project.cfcucaskyjpx.ap-southeast-2.rds.amazonaws.com',
+        'PORT': '5432'
     }
 }
 
